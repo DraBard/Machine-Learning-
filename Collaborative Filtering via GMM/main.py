e35fc648b3f89e8261a098b7b587c98b445cbc8c
drabard <palusba@gmail.com>
2021-11-16 22:38:21 +0100

    1st tak accomplished

--- Collaborative Filtering via GMM/main.py
+++ Collaborative Filtering via GMM/main.py
@@ -3,7 +3,22 @@ import kmeans
 import common
 import naive_em
 import em
+import matplotlib.pyplot as plt
 
 X = np.loadtxt("toy_data.txt")
 
-# TODO: Your code here
+# fig, axes = plt.subplots()
+# axes.scatter(X[:, 0], X[:, 1])
+# plt.show()
+
+# print(common.init(X, 1, 0))
+# print(kmeans.run(X, common.init(X, 1, 0)[0], common.init(X, 1, 0)[1]))
+
+for s in range(0, 5):
+    for k in range(1, 5):
+
+        Kmeans_mixture = kmeans.run(X, common.init(X, k, s)[0], common.init(X, k, s)[1])[0]
+        Kmeans_post = kmeans.run(X, common.init(X, k, s)[0], common.init(X, k, s)[1])[1]
+        print(kmeans.run(X, common.init(X, k, s)[0], common.init(X, k, s)[1])[2])
+
+        common.plot(X, Kmeans_mixture, Kmeans_post, "KMeans")
